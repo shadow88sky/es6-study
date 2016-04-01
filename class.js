70b716ec4b6dfc5306172f8acf2a756394725635
X.C <119136016@qq.com>
2016-04-01 22:24:32 +0800

    类

--- class.js
+++ class.js
@@ -1,4 +1,5 @@
 ﻿'use strict';
+//类定义
 class Polygon {
    constructor(height, width) {
       this.height = height;
@@ -6,4 +7,22 @@ class Polygon {
    }
 }
 
-console.log(123);
+//静态方法
+class Point {
+    constructor(x, y) {
+       this.x = x;
+       this.y = y;
+    }
+
+    static distance(a, b) {
+       const dx = a.x - b.x;
+       const dy = a.y - b.y;
+    
+       return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+console.log(Point.distance(p1, p2));
