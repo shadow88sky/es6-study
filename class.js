09ab21c7b6da2a43118a1c057ac000f111a5044d
X.C <119136016@qq.com>
2016-04-01 22:28:01 +0800

    继承

--- class.js
+++ class.js
@@ -26,3 +26,23 @@ const p1 = new Point(5, 5);
 const p2 = new Point(10, 10);
 
 console.log(Point.distance(p1, p2));
+
+
+//演示继承
+class Animal { 
+    constructor(name) {
+      this.name = name;
+    }
+
+    speak() {
+      console.log(this.name + ' makes a noise.');
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+      console.log(this.name + ' barks.');
+    }
+}
+var dog = new Dog('NiNi');
+dog.speak();
