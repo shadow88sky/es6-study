892a53b5347dd323b08c16bdc5fb8b7861b22aca
shadow88sky <119136016@qq.com>
2016-04-01 13:58:14 +0800

    let

---
+++ let.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var a = 5;
+var b = 10;
+if (a === 5) {
+    let a = 4; // The scope is inside the if-block
+    var b = 1; // The scope is inside the function
+    console.log(a);  // 4
+    console.log(b);  // 1
+}
+console.log(a); // 5
+console.log(b); // 1
+
+for (let i = 0; i < 10; i++) {
+    console.log(i); // 0, 1, 2, 3, 4 ... 9
+}
+console.log(i); // i is not defined
