1691e434e14ea8534d04e5a72d6a5e6f0f22cd59
X.C <119136016@qq.com>
2016-04-20 22:15:44 +0800

    promise

---
+++ promise.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+var fs = require('fs');
+var read = ()=> new Promise((resolve, reject) => {
+                      fs.readFile('./README.md', 'utf-8', (err, data) => {
+                      resolve(data);
+                      })
+})
+
+var read2 = ()=> new Promise((resolve, reject) => {
+fs.readFile('./class.js', 'utf-8', (err, data) => {
+resolve(data);
+})
+})
+
+////read().then((msg)=>{console.log(msg)});
+var test = function *(){
+    console.log('begin');
+    var data = yield read();
+    console.log(data);
+    var c = yield read2();
+    console.log(c);
+    console.log(123)
+}
+
+
+
+function run(generator) {
+    var it = generator();
+    
+    function go(result) {
+        if (result.done) return result.value;
+        
+        return result.value.then(function (value) {
+            return go(it.next(value));
+        }, function (error) {
+            return go(it.throw(value));
+        });
+    }
+    
+    go(it.next());
+}
+
+run(test);
